bafab240515e9bff6bf9eb6a7288f06ba5559f03
Olga Enygina <olga.enygina@bk.ru>
2023-01-29 21:06:56 +0300

    add tasks solutions

--- src/main/java/lesson2/HW.java
+++ src/main/java/lesson2/HW.java
@@ -1,12 +1,14 @@
 package lesson2;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class HW {
 
     public static void main(String[] args) {
 
-        System.out.println((istPalindrom(getString()))?"Палиндром":"Не палиндром");
+//        System.out.println((istPalindrom(getString()))?"Палиндром":"Не палиндром");
+        wordsToFile(WordRepeat());
 
     }
     public static String[] getString () {
@@ -31,6 +33,39 @@ public class HW {
             --last;
         }
         return true;
+    }
+
+    public static String WordRepeat() {
+        Scanner lineScanner = new Scanner(System.in);
+        String test = "TEST";
+        int n = -1;
+        do {
+            System.out.print("Введите числом, сколько раз напичатать слово TEST: ");
+            try {
+                n = lineScanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("Ошибка! ");
+                e.printStackTrace();
+                //log.error("Ошибка: {}", e.getMessage);
+            }
+        } while (n <= 0);
+        lineScanner.close();
+
+        String word =(test).repeat(n);
+        return word;
+    }
+
+    private static void wordsToFile (String word) {
+
+        try(FileWriter writer = new FileWriter("src/main/resources/lib/sem2hw3.txt", false))
+        {
+            writer.write(word);
+            System.out.println("Данные занесены в файл");
+            writer.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
